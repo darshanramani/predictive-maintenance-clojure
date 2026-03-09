2b1e797d6e7d003073f4f6e309d8b7724fe57154
Darshan Ramani <darshanramani@gmail.com>
2026-03-09 15:43:45 -0400

    add random forest

--- src/maintenance/core.clj
+++ src/maintenance/core.clj
@@ -1,16 +1,16 @@
 (ns maintenance.core
   (:gen-class)
   (:import
-    [java.io File]
-    [java.util Random]
-    [weka.core Instances]
-    [weka.core.converters CSVLoader]
-    [weka.filters Filter]
-    [weka.filters.unsupervised.attribute Remove]
-    [weka.filters.unsupervised.attribute StringToNominal]
-    [weka.filters.unsupervised.attribute NumericToNominal]
-    [weka.classifiers.trees J48]
-    [weka.classifiers Evaluation]))
+   [java.io File]
+   [java.util Random]
+   [weka.core Instances]
+   [weka.core.converters CSVLoader]
+   [weka.filters Filter]
+   [weka.filters.unsupervised.attribute Remove]
+   [weka.filters.unsupervised.attribute StringToNominal]
+   [weka.filters.unsupervised.attribute NumericToNominal]
+   [weka.classifiers Evaluation]
+   [weka.classifiers.trees J48 RandomForest]))
 
 (defn load-instances-from-csv [csv-path]
   (let [loader (CSVLoader.)]
@@ -32,38 +32,48 @@
 
 (defn apply-numeric-to-nominal [^Instances data attr-range]
   (let [f (NumericToNominal.)]
-    (.setAttributeIndices f attr-range) ; e.g. "7"
+    (.setAttributeIndices f attr-range)
     (.setInputFormat f data)
     (Filter/useFilter data f)))
 
 (defn accuracy [^Evaluation eval]
   (* 100.0 (- 1.0 (.errorRate eval))))
 
+(defn evaluate-model [model-name model data]
+  (let [eval (Evaluation. data)
+        rand (Random. 42)]
+    (.crossValidateModel eval model data 10 rand)
+    (println "----------------------------------")
+    (println model-name "using 10-fold Cross Validation")
+    (println "Accuracy (%):" (format "%.2f" (accuracy eval)))
+    (println "Confusion Matrix:")
+    (doseq [row (.confusionMatrix eval)]
+      (println (vec row)))
+    (println "----------------------------------")
+    (accuracy eval)))
+
 (defn -main [& _args]
   (println "Loading dataset...")
   (let [raw (load-instances-from-csv "data/ai4i2020.csv")]
     (println "Total rows loaded:" (.numInstances raw))
     (println "Total columns:" (.numAttributes raw))
-
-    ;; Remove leakage columns and IDs
     (let [step1 (apply-remove-filter raw "1-2,10-14")
-          ;; Now: 1 Type, 2 AirTemp, 3 ProcTemp, 4 RotSpeed, 5 Torque, 6 ToolWear, 7 Machine failure
-          step2 (apply-string-to-nominal step1 "1")   ; Type -> nominal
-          step3 (apply-numeric-to-nominal step2 "7")  ; Machine failure -> nominal
+          step2 (apply-string-to-nominal step1 "1")
+          step3 (apply-numeric-to-nominal step2 "7")
           _ (.setClassIndex step3 (dec (.numAttributes step3)))
-
-          model (J48.)
-          eval (Evaluation. step3)
-          rand (Random. 42)]
-
-      ;; 10-fold cross validation
-      (.crossValidateModel eval model step3 10 rand)
-
-      (println "----------------------------------")
-      (println "Decision Tree (J48) using 10-fold Cross Validation")
-      (println "Accuracy (%):" (format "%.2f" (accuracy eval)))
-      (println "Confusion Matrix:")
-      (doseq [row (.confusionMatrix eval)]
-        (println (vec row)))
-      (println "----------------------------------")
+          decision-tree (J48.)
+          random-forest (RandomForest.)
+          dt-acc (evaluate-model "Decision Tree (J48)" decision-tree step3)
+          rf-acc (evaluate-model "Random Forest" random-forest step3)]
+      (.buildClassifier decision-tree step3)
+      (println "Decision Tree Structure:")
+      (println decision-tree)
+      (println "Final Model Comparison")
+      (println "Decision Tree Accuracy (%):" (format "%.2f" dt-acc))
+      (println "Random Forest Accuracy (%):" (format "%.2f" rf-acc))
+      (if (> rf-acc dt-acc)
+        (println "Random Forest performed better than Decision Tree.")
+        (if (< rf-acc dt-acc)
+          (println "Decision Tree performed better than Random Forest.")
+          (println "Both models performed equally.")))
       (println "Done"))))
